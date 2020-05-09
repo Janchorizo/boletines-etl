0ff5703e05b89705693749cddedc495d8308cdad
Janchorizo <jancho@usal.es>
2020-05-09 18:01:27 +0200

    Apply PEP8 and PEP257.

--- src/helpers/boe_diary_processing.py
+++ src/helpers/boe_diary_processing.py
@@ -1,3 +1,5 @@
+"""Utility methods for BOE diary processing."""
+
 from typing import Dict, Generator, Iterable
 import datetime
 import collections
@@ -8,43 +10,55 @@ import httplib2
 from . import helpers
 from . import boe
 
-def get_sections(tree) -> Generator:
-    sections = ((section.get(boe.SummaryAttribute.section_number), section)
-            for section
-            in helpers.use_tree_for_search(tree)(boe.SummaryXpath.section))
-    
+
+def get_sections(tree: et._Element) -> Generator:
+    """Get sections' node and number in a BOE diary."""
+    sections = (
+        (section.get(boe.SummaryAttribute.section_number), section)
+        for section
+        in helpers.use_tree_for_search(tree)(boe.SummaryXpath.section))
+
     return sections
 
-def get_departments_per_section(sections) -> Generator:
+
+def get_departments_per_section(sections: Iterable) -> Generator:
+    """Get departments' node and name in a BOE diary section."""
     departments = (
-            (section_number, department.get(boe.SummaryAttribute.department_name), department)
-            for (section_number, section)
-            in sections
-            for department
-            in helpers.use_tree_for_search(section)(boe.SummaryXpath.department))
-    
+        (section_number, department.get(boe.SummaryAttribute.department_name), department)
+        for (section_number, section)
+        in sections
+        for department
+        in helpers.use_tree_for_search(section)(boe.SummaryXpath.department))
+
     return departments
 
-def get_items_per_department(departments) -> Generator:
-    items = ((section_number, department_name, item)
+
+def get_items_per_department(departments: Iterable) -> Generator:
+    """Get all items (including epigraphs) in a BOE diary department."""
+    items = (
+        (section_number, department_name, item)
         for (section_number, department_name, department)
         in departments
         for item
         in helpers.use_tree_for_search(department)(boe.SummaryXpath.items))
-    
+
     return items
 
-def get_item_details(section_number:str, department_name:str, node) -> Dict:
+
+def get_item_details(section_number: str, department_name: str, node: et._Element) -> Dict:
+    """Extract a BOE diary entry's deatils."""
     search_details = helpers.use_tree_for_search(node)
     title_node = search_details(boe.SummaryXpath.item_title)[0]
     pdf_url_node = search_details(boe.SummaryXpath.item_pdf_url)[0]
     xml_url_node = search_details(boe.SummaryXpath.item_xml_url)[0]
     htm_url_node = search_details(boe.SummaryXpath.item_htm_url)[0]
-    
+
     parent = node.getparent()
     is_epigraph = parent.tag.lower() == 'epigrafe'
-    epigraph = parent = parent.get(boe.SummaryAttribute.epigraph_name) if is_epigraph else ''
-    
+    epigraph = parent.get(boe.SummaryAttribute.epigraph_name) \
+        if is_epigraph \
+        else ''
+
     details = {}
     details['id'] = node.get(boe.SummaryAttribute.item_id)
     details['epigraph'] = epigraph
@@ -54,11 +68,12 @@ def get_item_details(section_number:str, department_name:str, node) -> Dict:
     details['pdf_url'] = pdf_url_node.text
     details['xml_url'] = xml_url_node.text
     details['htm_url'] = htm_url_node.text
-    
+
     return details
 
-def get_details_per_item(items) -> Generator:
+
+def get_details_per_item(items: Iterable) -> Generator:
+    """Get the details for each provided BOE diary item."""
     details = (get_item_details(*item) for item in items)
-    
-    return details
 
+    return details
