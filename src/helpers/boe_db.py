cd0cf117eb0ca028992133fb5e6ebb925e348235
Janchorizo <jancho@usal.es>
2021-02-05 12:02:30 +0100

    Add economic impact to expected db parameters.

--- src/helpers/boe_db.py
+++ src/helpers/boe_db.py
@@ -1,10 +1,11 @@
 def boe_diary_entry_query(item):
     return f"""
         INSERT INTO boe_diary_entry
-        (id, date, title, section, department, epigraph, pdf_url, xml_url, htm_url)
+        (id, date, title, section, department, epigraph, pdf_url, xml_url, htm_url, economic_impact)
         VALUES ('{item.get('id')}', '{item.get('date')}', '{item.get('title')}', 
                 '{item.get('section')}', '{item.get('department')}', '{item.get('epigraph')}',
-                '{item.get('pdf_url')}', '{item.get('xml_url')}', '{item.get('htm_url')}');
+                '{item.get('pdf_url')}', '{item.get('xml_url')}', '{item.get('htm_url')}',
+				'{item.get('economic_impact')}');
     """
 
 def boe_diary_entry_is_valid(item):
@@ -17,8 +18,9 @@ def boe_diary_entry_is_valid(item):
 		'epigraph', 
 		'pdf_url', 
 		'xml_url', 
-		'htm_url'
+		'htm_url',
+		'economic_impact'
 	)
-	print(item)
+
 	keys_are_present = all(map(lambda key: key in item.keys(), expected_keys))
 	return keys_are_present
