697bc8b88f06efad907f0e229f3162058a84f9ba
Janchorizo <jancho@usal.es>
2021-02-05 15:29:08 +0100

    Change how local pipeline is evaluated.

--- src/pipelines/local_no_indexing_boe_pipeline.py
+++ src/pipelines/local_no_indexing_boe_pipeline.py
@@ -9,19 +9,29 @@ from tasks.save_boe_diary_entry import SaveBoeDiaryEntry, SaveOption
 class Pipeline(luigi.WrapperTask):
     date = luigi.DateParameter()
 
+    @classmethod
+    def bulk_complete(cls, parameter_tuples):
+        return tuple()
+
     def complete(self):
-        return False
+        completed = True
+        completed = completed and ProcessBoeDiary(date=self.date).complete()
+        completed = completed and all(t.complete() for t in self.get_entry_tasks())
+        return completed
 
     def requires(self):
         return ProcessBoeDiary(date=self.date)
 
-    def run(self):
+    def get_entry_tasks(self):
         with self.input().open('r') as f:
             diary_entries = json.loads(f.read())
         
-        yield (SaveBoeDiaryEntry(entry=entry, save_options=(SaveOption.DATABASE.value,))
+        return (SaveBoeDiaryEntry(entry=entry, save_options=(SaveOption.DATABASE.value,))
                for entry 
                in diary_entries)
+
+    def run(self):
+        yield self.get_entry_tasks()
 
 if __name__ == '__main__':
     print(['Pipeline',
