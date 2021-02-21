77a948a0eadc5a08efcc288568dc455e2fa5b39d
Janchorizo <jancho@usal.es>
2021-02-21 09:14:09 +0100

    Add entry type and type description to query data.

--- src/helpers/boe_db.py
+++ src/helpers/boe_db.py
@@ -1,12 +1,22 @@
 def boe_diary_entry_query(item):
-    return f"""
+    query = f"""
         INSERT INTO boe_diary_entry
-        (id, date, title, section, department, epigraph, pdf_url, xml_url, htm_url, economic_impact)
-        VALUES ('{item.get('id')}', '{item.get('date')}', '{item.get('title')}', 
-                '{item.get('section')}', '{item.get('department')}', '{item.get('epigraph')}',
-                '{item.get('pdf_url')}', '{item.get('xml_url')}', '{item.get('htm_url')}',
-				'{item.get('economic_impact')}');
+        (id, date, title, section, department, epigraph, pdf_url, xml_url, htm_url, economic_impact, type, type_desc)
+        VALUES (
+            '{item.get('id')}',
+            '{item.get('date')}',
+            '{item.get('title')}',
+            '{item.get('section')}',
+            '{item.get('department')}',
+            '{item.get('epigraph')}',
+            '{item.get('pdf_url')}',
+            '{item.get('xml_url')}',
+            '{item.get('htm_url')}',
+            '{item.get('economic_impact')}',
+            '{item.get('type')}',
+            '{item.get('type_desc')}');
     """
+    return query
 
 def boe_diary_entry_is_valid(item):
 	expected_keys = (
@@ -19,7 +29,9 @@ def boe_diary_entry_is_valid(item):
 		'pdf_url', 
 		'xml_url', 
 		'htm_url',
-		'economic_impact'
+		'economic_impact',
+		'type',
+		'type_desc'
 	)
 
 	keys_are_present = all(map(lambda key: key in item.keys(), expected_keys))
