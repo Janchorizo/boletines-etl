f907677644f0576e4030199580ec1772d7ab8f44
Janchorizo <jancho@usal.es>
2021-02-22 10:20:01 +0100

    Add formatted section name mapping.

--- src/helpers/boe.py
+++ src/helpers/boe.py
@@ -105,6 +105,20 @@ code_to_section_name = {
     't': 'tribunal constitucional'
 }
 
+code_to__formatted_section_name = {
+    '1': 'disposiciones generales',
+    '2': 'autoridades y personal',
+    '2a': 'nombramientos situaciones    \ne incidencias',
+    '2b': 'oposiciones y concursos',
+    '3': 'otras secciones',
+    '4': 'administración de justicia',
+    '5': 'anuncios',
+    '5a': 'licitaciones públicas    \ny adjudicaciones',
+    '5b': 'otros anuncios particulares',
+    '5c': 'anuncios particulares',
+    't': 'tribunal constitucional'
+}
+
 code_to_modality_name = {
     'L': 'Licitación',
     'F': 'Formalización contrato',
