8336e8aebefbcc71e431ecc35a24ba8b15a16043
Janchorizo <jancho@usal.es>
2021-03-01 12:17:11 +0100

    Update data pipeline to create and save dash data.

--- src/pipelines/app_data_pipeline.py
+++ src/pipelines/app_data_pipeline.py
@@ -6,19 +6,24 @@ import pymysql
 
 from pipelines import no_indexing_boe_pipeline
 from tasks.save_boe_diary_summary import SaveBoeDiarySummary
+from tasks.save_dash_data import SaveDashData
 
 
 class AppDataPipeline(luigi.WrapperTask):
     date = luigi.DateParameter()
 
     def complete(self):
-        return SaveBoeDiarySummary(date=self.date).complete()
+        return all((
+            SaveBoeDiarySummary(date=self.date).complete(),
+            SaveDashData(date=self.date).complete()
+        ))
 
     def requires(self):
-        return no_indexing_boe_pipeline.Pipeline(date=self.date)
+        return None #no_indexing_boe_pipeline.Pipeline(date=self.date)
 
     def run(self):
         yield SaveBoeDiarySummary(date=self.date)
+        yield SaveDashData(date=self.date)
 
 if __name__ == '__main__':
     luigi.run(['Pipeline',
